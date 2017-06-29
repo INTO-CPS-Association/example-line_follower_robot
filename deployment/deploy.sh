11c811902c5daa32ccfd983e7eeb5600d5cb6da4
Victor Bandur <victor.bandur@eng.au.dk>
2017-06-29 22:38:41 +0200

    Deployment script WIP.

---
+++ deployment/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+leftval=`grep 'name="lfLeftVal"' modelDescription.xml | awk -F \" '{print $4}'`
+forwardrotate=`grep 'name="forwardRotate"' modelDescription.xml | awk -F \" '{print $4}'`
+servoleft=`grep 'name="servoLeftVal"' modelDescription.xml | awk -F \" '{print $4}'`
+servoright=`grep 'name="servoRightVal"' modelDescription.xml | awk -F \" '{print $4}'`
+forwardspeed=`grep 'name="forwardSpeed"' modelDescription.xml | awk -F \" '{print $4}'`
+rightval=`grep 'name="lfRightVal"' modelDescription.xml | awk -F \" '{print $4}'`
+backwardrotate=`grep 'name="backwardRotate"' modelDescription.xml | awk -F \" '{print $4}'`
+
+cp main-template.c main.c
+
+sed -i "s/XX_leftval_XX/$leftval/g" main.c
+sed -i "s/XX_forwardrotate_XX/$forwardrotate/g" main.c
+sed -i "s/XX_servoleft_XX/$servoleft/g" main.c
+sed -i "s/XX_servoright_XX/$servoright/g" main.c
+sed -i "s/XX_forwardspeed_XX/$forwardspeed/g" main.c
+sed -i "s/XX_rightval_XX/$rightval/g" main.c
+sed -i "s/XX_backwardrotate_XX/$backwardrotate/g" main.c
