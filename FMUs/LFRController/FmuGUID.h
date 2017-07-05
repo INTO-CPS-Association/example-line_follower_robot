8fc981819fb0d1a4701264adfda38b1446a8c31e
Kenneth Lausdahl <lausdahl@eng.au.dk>
2017-07-05 10:51:43 +0300

    cleanup

--- FMUs/LFRController/FmuGUID.h
+++
@@ -1,6 +0,0 @@
-#ifndef FMU_GUID_H_
-#define FMU_GUID_H_
-
-#define _FMU_GUID "{95e08c3a-dd6f-4853-9f80-e3473157c261}"
-
-#endif /* FMU_GUID_H_ */
--- FMUs/Sensor_Block/sources/fmiGUID.h
+++
@@ -1,1 +0,0 @@
-#define FMI_GUID "{055a4706-7749-41de-a1ab-7d6d28a95c69}"
